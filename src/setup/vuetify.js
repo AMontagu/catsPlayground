a1d67909ca6eae7f8fc387bb9f97f7cc6913a6c0
Unknown <adrienmontagu@gmail.com>
2019-03-09 14:16:29 +0100

    integrate vuetify

---
+++ src/setup/vuetify.js
@@ -0,0 +1,36 @@
+import Vue from 'vue';
+import Vuetify, {
+	VApp, // required
+	VNavigationDrawer,
+	VFooter,
+	VToolbar,
+	VFadeTransition,
+	VFlex,
+	VLayout,
+	VContent,
+	VToolbarTitle,
+	VBtn,
+	VContainer,
+	VSpacer,
+	VImg
+} from "vuetify/lib";
+
+export default () => {
+	Vue.use(Vuetify, {
+		components: {
+			VApp,
+			VNavigationDrawer,
+			VFooter,
+			VToolbar,
+			VFadeTransition,
+			VFlex,
+			VLayout,
+			VContent,
+			VToolbarTitle,
+			VBtn,
+			VContainer,
+			VSpacer,
+			VImg
+		}
+	});
+}
